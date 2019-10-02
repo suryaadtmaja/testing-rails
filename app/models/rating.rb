d20dc22e6bbdea690f3c296e566258cdaa2d5213
Filsuck <suryaadtmaja13@gmail.com>
2019-10-02 21:06:51 +0700

    finish first test

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,4 @@
 class Rating < ApplicationRecord
   belongs_to :person
+  validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -5,7 +5,22 @@ class RatingTest < ActiveSupport::TestCase
   #   assert true
   # end
   test "valid if score between 0 and 10" do
-    rating = Rating.new(score: 10)
-    assert rating.valid?
+    0.upto(10).each do |i|
+      rating = Rating.new(score: i)
+      rating.valid?
+      assert_empty rating.errors[:score]
+    end
+  end
+
+  test "invalid if score is less than 0" do
+    rating = Rating.new(score: -1)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
+  end
+
+  test "invalid if score is more than 10" do
+    rating = Rating.new(score: 11)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
   end
 end
